c4efa4463cf6fb37c3440c7bd8b12a31ecca98ec
Your NameGIT <you example.com>
2019-06-20 19:49:48 +0300

    chtoto

---
+++ Car.java
@@ -0,0 +1,40 @@
+package JavaProjects;
+
+public class Car {
+
+    public static void main(String args[]){
+        int wheels;
+        double engineCapacity;
+        public Car(){
+            wheels=4;
+            engineCapacity=1,0;
+        }
+        public Car(int Wheels,double capacity){
+            this.wheels = wheels;
+            engineCapacity = capacity;
+        }
+
+        public class New Car {
+            int wheels;
+            double engineCapacity;
+            double CarBrand;
+            double CarColor;
+            double WhroomWhroom;
+            public NewCar(){
+                wheels=2;
+                engineCapacity=6,0;
+                CarColor=black;
+                WhroomWhroom=yes;
+                CarBrand=Toyota20veka;
+                if[Car wheels=2;
+                   CarColor=black;
+                   CarBrand=Toyota;
+                   else{
+                   system.out.println("ахуеть спасибо папаша что купил мне тайоту черного цвета 20го века")}
+
+                        ]
+
+            }
+        }
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    static int myAge = 16;
+    public static void main(String[] args) {
+        System.out.println("Hello World"+"My age is " + (myAge+6));
+        second();
+    }
+
+    public static void second()
+    {
+        System.out.println("**********Hello from second method")
+    }
+}
